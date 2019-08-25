33c09189dff1b0e27ed2cd2a5d309eb9e5cc6def
alete89 <alejandrodini@mural.co>
2019-08-25 03:14:28 -0300

    feat: create Moneda test endpoint

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,6 @@
-import express from "express"
+import express, { Request, Response } from "express"
+import { createConnection } from "typeorm"
+import { Moneda } from "../model/moneda"
 
 // Create a new express application instance
 const app: express.Application = express()
@@ -7,6 +9,23 @@ app.get("/", function(req, res) {
     res.send("Welcome TEST")
 })
 
+app.get("/monedas", async function(req: Request, res: Response) {
+    const monedas = await Moneda.find()
+    res.send(monedas)
+})
+
 app.listen(3000, function() {
     console.log("Example app listening on port 3000!")
 })
+
+async function run() {
+    await createConnection()
+}
+
+function createTest() {
+    const moneda = new Moneda()
+    moneda.nombre = "pesito"
+    moneda.save()
+}
+
+run().then(() => createTest())

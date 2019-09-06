36aec2f5976afa7b03234029b47eb53659971fed
nicovio <nicolasviotti90@gmail.com>
2019-09-06 16:11:24 -0300

    fix: rules from 1 to n instead of 0 to n

--- src/model/tarjeta.ts
+++ src/model/tarjeta.ts
@@ -18,21 +18,20 @@ export class Tarjeta extends BaseEntity {
     gastos: Gasto[]
 
     @Column()
-    dia_regla_cierre_resumen: number // día dentro de la semana 0-6
+    dia_regla_cierre_resumen: number // día dentro de la semana 1-7 (LUNES -> 1)
 
     @Column()
-    semana_regla_cierre_resumen: number // semana dentro de un mes 0-3
+    semana_regla_cierre_resumen: number // semana dentro de un mes 1-4
 
     @Column()
-    dia_regla_vencimiento: number // día dentro de la semana 0-6
+    dia_regla_vencimiento: number // día dentro de la semana 1-7 (LUNES -> 1)
 
     @Column()
-    semana_regla_vencimiento: number // semana dentro de un mes 0-3
+    semana_regla_vencimiento: number // semana dentro de un mes 1-4
 
     fechaDeCierre(anio: number, mes: number) {
         let fecha = new Date(anio, mes - 1, 1)
-        const diferencia_dia_regla_primer_dia_mes = this.dia_regla_cierre_resumen - fecha.getDay()
-        fecha.setDate(fecha.getDate() + 7 * this.semana_regla_cierre_resumen + this.ajusteDia(diferencia_dia_regla_primer_dia_mes))
+        fecha.setDate(1 + 7 * (this.semana_regla_cierre_resumen - 1) + this.ajusteDia(this.getDayFrom0To6(this.dia_regla_cierre_resumen) - fecha.getDay()))
         if (fecha.getMonth() > mes - 1) fecha.setDate(fecha.getDate() - 7)  //EN EL CASO DE QUE LA REGLA SEA LA 5TA SEMANA Y ESE DIA TIENE SOLO 4 EN ESE MES, LE RESTA 7 DIAS PARA DEVOLVER EL 4TO.
         return fecha
     }
@@ -42,5 +41,10 @@ export class Tarjeta extends BaseEntity {
             return 7 + diferencia
         else
             return diferencia
+    }
+
+    getDayFrom0To6(dia: number) {
+        if (dia == 7) return 0
+        else return dia
     }
 }

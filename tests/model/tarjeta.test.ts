8047c05e1711023f0fc032fa283941df0a77b838
nicovio <nicolasviotti90@gmail.com>
2019-09-06 16:11:58 -0300

    fix: tests

--- tests/model/tarjeta.test.ts
+++ tests/model/tarjeta.test.ts
@@ -4,20 +4,20 @@ import "mocha"
 
 describe("el cuarto jueves de septiembre 2019", () => {
     const tarjeta = new Tarjeta()
-    tarjeta.dia_regla_cierre_resumen = 3 // jueves
+    tarjeta.dia_regla_cierre_resumen = 4 // jueves
     tarjeta.semana_regla_cierre_resumen = 4 // cuarta semana
     it("debe ser el 26 de septiembre", () => {
         const result = tarjeta.fechaDeCierre(2019, 9) // septiembre
-        expect(result).to.be.equal(new Date("2019-09-26"))
+        expect(result.toString()).to.be.equal(new Date("26 September 2019").toString())
     })
 })
 
 describe("el primer lunes de febrero 2019", () => {
     const tarjeta = new Tarjeta()
-    tarjeta.dia_regla_cierre_resumen = 0 // lunes
-    tarjeta.semana_regla_cierre_resumen = 1 // primera semana
+    tarjeta.dia_regla_cierre_resumen = 1 // lunes
+    tarjeta.semana_regla_cierre_resumen = 1// primera semana
     it("debe ser el 4 de febrero", () => {
         const result = tarjeta.fechaDeCierre(2019, 2) // febrero
-        expect(result).to.equal(new Date("2019-02-04"))
+        expect(result.toString()).to.equal(new Date("4 February 2019").toString())
     })
 })

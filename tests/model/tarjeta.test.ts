78fd105728d5d077fd1169efe99a316460f27677
alete89 <alejandrodini@mural.co>
2019-09-06 00:36:03 -0300

    fix: tests

--- tests/model/tarjeta.test.ts
+++ tests/model/tarjeta.test.ts
@@ -2,21 +2,21 @@ import { Tarjeta } from "../../src/model/tarjeta"
 import { expect } from "chai"
 import "mocha"
 
-describe("el cuarto jueves de septiembre", () => {
+describe("el cuarto jueves de septiembre 2019", () => {
     const tarjeta = new Tarjeta()
-    tarjeta.dia_regla_cierre_resumen = 4 // jueves
-    tarjeta.semana_regla_cierre_resumen = 3 // cuarta semana
-    it("debe ser el fecha 26", () => {
+    tarjeta.dia_regla_cierre_resumen = 3 // jueves
+    tarjeta.semana_regla_cierre_resumen = 4 // cuarta semana
+    it("debe ser el 26 de septiembre", () => {
         const result = tarjeta.fechaDeCierre(2019, 9) // septiembre
         expect(result).to.be.equal(new Date("2019-09-26"))
     })
 })
 
-describe("el primer lunes de febrero", () => {
+describe("el primer lunes de febrero 2019", () => {
     const tarjeta = new Tarjeta()
-    tarjeta.dia_regla_cierre_resumen = 1 // lunes
-    tarjeta.semana_regla_cierre_resumen = 0 // primera semana
-    it("debe ser el fecha 4", () => {
+    tarjeta.dia_regla_cierre_resumen = 0 // lunes
+    tarjeta.semana_regla_cierre_resumen = 1 // primera semana
+    it("debe ser el 4 de febrero", () => {
         const result = tarjeta.fechaDeCierre(2019, 2) // febrero
         expect(result).to.equal(new Date("2019-02-04"))
     })

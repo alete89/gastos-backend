9540c784d0c6f1dbba8c270037339bd1121b5144
nicovio <nicolasviotti90@gmail.com>
2019-09-09 23:55:08 -0300

    fix: tests

--- tests/model/tarjeta.test.ts
+++ tests/model/tarjeta.test.ts
@@ -7,8 +7,8 @@ describe("el cuarto jueves de septiembre 2019", () => {
     tarjeta.dia_regla_cierre_resumen = 4 // jueves
     tarjeta.semana_regla_cierre_resumen = 4 // cuarta semana
     it("debe ser el 26 de septiembre", () => {
-        const result = tarjeta.fechaDeCierre(2019, 9) // septiembre
-        expect(result.toString()).to.be.equal(new Date("26 September 2019").toString())
+        const result = tarjeta.calcularfechaDeCierre(2019, 8) // septiembre (Enero es 0)
+        expect(result.toString()).to.be.equal(new Date("26 September 2019 23:59:59").toString())
     })
 })
 
@@ -17,7 +17,7 @@ describe("el primer lunes de febrero 2019", () => {
     tarjeta.dia_regla_cierre_resumen = 1 // lunes
     tarjeta.semana_regla_cierre_resumen = 1// primera semana
     it("debe ser el 4 de febrero", () => {
-        const result = tarjeta.fechaDeCierre(2019, 2) // febrero
-        expect(result.toString()).to.equal(new Date("4 February 2019").toString())
+        const result = tarjeta.calcularfechaDeCierre(2019, 1) // febrero (Enero es 0)
+        expect(result.toString()).to.equal(new Date("4 February 2019 23:59:59").toString())
     })
 })

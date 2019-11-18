25b6d9e8ddb01e02b6327e0f415fb7cfa902b6c3
nicovio <nicolasviotti90@gmail.com>
2019-11-18 03:11:30 -0300

    formatting

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,36 +6,37 @@ import { Tag } from '../model/tag'
 import { Tarjeta } from '../model/tarjeta'
 import { Bootstrap } from './bootstrap'
 import bodyParser from 'body-parser'
+import { Sumary } from '../model/sumary'
 
 // Create a new express application instance
 const app: express.Application = express()
-// const bootstrap: Bootstrap = new Bootstrap()
+const bootstrap: Bootstrap = new Bootstrap()
 
 app.use(express.json()) // to support JSON-encoded bodies
 app.use(express.urlencoded({ extended: true })) // to support URL-encoded bodies
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*') // allow requests from any other server
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE') // allow these verbs
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Cache-Control')
     next()
 })
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.send('Welcome TEST')
 })
 
-app.get('/monedas', async function(req: Request, res: Response) {
+app.get('/monedas', async function (req: Request, res: Response) {
     const monedas = await Moneda.find()
     res.send(monedas)
 })
 
-app.get('/gastos', async function(req: Request, res: Response) {
+app.get('/gastos', async function (req: Request, res: Response) {
     const gastos = await Gasto.find({ relations: ['tags', 'moneda', 'tarjeta'] })
     res.send(gastos)
 })
 
-app.put('/gastos/mes', async function(req: Request, res: Response) {
+app.put('/gastos/mes', async function (req: Request, res: Response) {
     const fechaABuscar: string = formatearFecha(new Date(req.body.anio, req.body.mes, 1))
     const gastos = await Gasto.find({
         relations: ['tags', 'moneda', 'tarjeta'],
@@ -45,12 +46,12 @@ app.put('/gastos/mes', async function(req: Request, res: Response) {
     res.send(gastos)
 })
 
-app.get('/tags', async function(req: Request, res: Response) {
+app.get('/tags', async function (req: Request, res: Response) {
     const tags = await Tag.find()
     res.send(tags)
 })
 
-app.post('/tags/new', async function(req: Request, res: Response) {
+app.post('/tags/new', async function (req: Request, res: Response) {
     if (req.body.nombre) {
         const tag = new Tag({ nombre: req.body.nombre })
         try {
@@ -64,7 +65,7 @@ app.post('/tags/new', async function(req: Request, res: Response) {
     }
 })
 
-app.get('/anios/:id_tarjeta', async function(req: Request, res: Response) {
+app.get('/anios/:id_tarjeta', async function (req: Request, res: Response) {
     if (req.params.id_tarjeta) {
         const response = await Tarjeta.query(
             `SELECT MIN(YEAR(fecha_primer_resumen)) as desde,
@@ -80,12 +81,12 @@ app.get('/anios/:id_tarjeta', async function(req: Request, res: Response) {
     }
 })
 
-app.get('/tarjetas', async function(req: Request, res: Response) {
+app.get('/tarjetas', async function (req: Request, res: Response) {
     const tarjetas = await Tarjeta.find({ relations: ['gastos'] })
     res.send(tarjetas)
 })
 
-app.post('/gasto', async function(req: Request, res: Response) {
+app.post('/gasto', async function (req: Request, res: Response) {
     try {
         const gasto = new Gasto(req.body)
         gasto.tarjeta = await Tarjeta.findOneOrFail(req.body.tarjeta)
@@ -104,7 +105,7 @@ app.post('/gasto', async function(req: Request, res: Response) {
  * Returns an array with three elements (last month, this month, next month)
  * each one is an array with the sum of all the gastos of the month for each Credit card in the system
  */
-app.get('/summary', async function(req: Request, res: Response) {
+app.get('/summary', async function (req: Request, res: Response) {
     const tarjetas = await Tarjeta.find({ relations: ['gastos'] })
     const hoy = new Date()
     const meses = [
@@ -116,7 +117,7 @@ app.get('/summary', async function(req: Request, res: Response) {
     res.send(result)
 })
 
-app.listen(3000, function() {
+app.listen(3000, function () {
     console.log('Gastos backend listening on port 3000!')
 })
 
@@ -124,9 +125,9 @@ async function run() {
     await createConnection()
 }
 
-// async function runBootstrap() {
-//     await bootstrap.run()
-// }
+async function runBootstrap() {
+    await bootstrap.run()
+}
 
 function formatearFecha(fecha: Date) {
     return `${fecha.toISOString().slice(0, 10)} 00:00:00`

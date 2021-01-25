9323ed1d2aacf1d0904bc20fded5ee15ec9cd3a3
nicovio <nicolasviotti90@gmail.com>
2021-01-24 23:03:58 -0300

    ssl solo en produccion

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,6 +1,11 @@
-const entities = process.env.NODE_ENV === 'production'
+const isProductionEnv = () => process.env.NODE_ENV === 'production'
+
+const extra = isProductionEnv() ? { ssl: { rejectUnauthorized: false } } : {}
+
+const entities = isProductionEnv()
   ? ['build/src/model/*.js']
   : ['src/model/*.ts']
+
 
 module.exports = {
   type: 'postgres',
@@ -8,10 +13,6 @@ module.exports = {
     process.env.DATABASE_URL || process.env.DEV_DATABASE_URL,
   entities,
   synchronize: true,
-  ssl: true,
-  extra: {
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  },
+  ssl: isProductionEnv(),
+  extra
 };
